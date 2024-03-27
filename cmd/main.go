9b184459fc1084d782a6d15ee2c73a240b4cb8e7
gvengerov <gvengerov@ozon.ru>
2024-03-27 11:51:26 +0300

    clean arcitecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	todo "github.com/qasimiss/http-rest-todo"
 	"github.com/qasimiss/http-rest-todo/pkg/handler"
+	"github.com/qasimiss/http-rest-todo/pkg/repository"
+	"github.com/qasimiss/http-rest-todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocured while running http server: %s", err.Error())
